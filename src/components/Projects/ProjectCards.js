8375e3803c167eea1d9a19e228e4fa3288218b2e
SayyidNurCahyo <sayyidnurc123@gmail.com>
2026-05-28 18:48:43 +0700

    update

--- src/components/Projects/ProjectCards.js
+++ src/components/Projects/ProjectCards.js
@@ -1,22 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
 import { CgWebsite } from "react-icons/cg";
-import { BsGithub } from "react-icons/bs";
+import { BsChevronLeft, BsChevronRight, BsGithub } from "react-icons/bs";
 
 function ProjectCards(props) {
+  const [activeImage, setActiveImage] = useState(0);
   return (
     <Card className="project-card-view">
-      <Card.Img variant="top" src={props.imgPath} alt="card-img" />
+      <div className="project-image">
+        <Button variant="primary" onClick={() => setActiveImage(activeImage - 1)} disabled={activeImage === 0}>
+          <BsChevronLeft /> &nbsp;
+        </Button>
+        <Card.Img variant="top" src={props.imgPath[activeImage]} alt="card-img" />
+        <Button variant="primary" onClick={() => setActiveImage(activeImage + 1)} disabled={activeImage === props.imgPath.length - 1}>
+          <BsChevronRight /> &nbsp;
+        </Button>
+      </div>
       <Card.Body>
         <Card.Title>{props.title}</Card.Title>
         <Card.Text style={{ textAlign: "justify" }}>
           {props.description}
         </Card.Text>
-        <Button variant="primary" href={props.ghLink} target="_blank">
-          <BsGithub /> &nbsp;
-          {props.isBlog ? "Blog" : "GitHub"}
-        </Button>
+        {props.ghLink && (
+          <Button variant="primary" href={props.ghLink} target="_blank">
+            <BsGithub /> &nbsp;
+            {props.isBlog ? "Blog" : "GitHub"}
+          </Button>
+        )}
         {"\n"}
         {"\n"}
 
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -5,10 +5,14 @@ import Particle from "../Particle";
 import webApp2 from "../../Assets/Projects/e-commerce-web.png";
 import webApp4 from "../../Assets/Projects/eazycamp-web.png";
 import webApp6 from "../../Assets/Projects/timesheet-management.png";
-import data2 from "../../Assets/Projects/anemia-interface.png";
+import webApp7 from "../../Assets/Projects/ajinomoto-web-example.png";
+import webApp8 from "../../Assets/Projects/ajinomoto-desktop-example.jpeg";
+import webApp9 from "../../Assets/Projects/takasago-web-example.jpeg";
+import webApp10 from "../../Assets/Projects/takasago-web-example2.jpeg";
+import prisma1 from "../../Assets/Projects/prisma-desktop-example.jpeg"
+import prisma2 from "../../Assets/Projects/prisma-mobile-example.jpeg"
 import data3 from "../../Assets/Projects/cnn-all.png";
 import data4 from "../../Assets/Projects/covid-rfsvm.png";
-import data6 from "../../Assets/Projects/weather-nn.png";
 
 function Projects() {
   return (
@@ -24,9 +28,39 @@ function Projects() {
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={webApp6}
+              imgPath={[prisma1, prisma2]}
               isBlog={false}
-              title="Timesheet Management & Approval Website & API"
+              title="Multi-platform Modular Integrated Weighing App"
+              description="Build integrated weighing application with modular monolith architecture that run into Desktop & Android platform. Use Vue and Tauri bundler with dynamic import & active module script to maintain modularity of feature into a single app with clean logic separation. Have server API integration, background internal API, scale & printer microservices built with Go & gomobile, responsive Tailwind UI with reusable component plugin. This app provide 3 module (until now) integration, used by many company customers and also as an internal demo app for sales"
+              demoLink=""
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={[webApp7, webApp8]}
+              isBlog={false}
+              title="Ajinomoto Sajiku Weighing System"
+              description="Build API, Website and Desktop Application for Sajiku Weighing System Production using Go Gin, Vue, and Tauri bundler with raw material & stock management feature, continues production in weeks, also provide websocket, printer & scale microservices. This system success to make production faster, reliable and traceable with easier stock data collection, managing 50 user concurrent daily with background queue job, easier reporting with 6 kind of report provided, also desktop integration with barcode scanner, scale & IOT module"
+              demoLink=""
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={[webApp9, webApp10]}
+              isBlog={false}
+              title="Takasago Laboratory Weighing System"
+              description="Build API & Website for Takasago Laboratory Weighing System using Go Gin & Vue that accomodate access from mobile app, SAP integration, websocket & scale microservices. This website success to make daily task of weighing lab easier with production synchronization with SAP, reporting into excel & pdf, and inventory management"
+              demoLink=""
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={[webApp6]}
+              isBlog={false}
+              title="Timesheet Management & Approval Website & API" 
               description="Build Timesheet management and approval website for administration purpose in company using Go Gin & Angular that include services: security & error prevention, CRUD, email confirmation and activation link, cloud image, postman collection testing, basic auth in web and generate id from token"
               ghLink="https://github.com/SayyidNurCahyo/goGin-TimesheetAPI"
               demoLink=""
@@ -35,7 +69,7 @@ function Projects() {
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={webApp2}
+              imgPath={[webApp2]}
               isBlog={false}
               title="Restaurant Management Application"
               description="Build fullstack web application for user or admin to manage their product, table in restaurant, transaction type (dine in or take away) in restaurant using Java Spring and React JS framework that implement CRUD, security, relational database, image uploading, clean architecture, midtrans payment, unit testing, and manage transaction"
@@ -46,7 +80,7 @@ function Projects() {
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={webApp4}
+              imgPath={[webApp4]}
               isBlog={false}
               title="EazyCamp Web & Mobile App"
               description="Build fullstack application for renting camping and hiking tools that connect admin, customer, and guider based on camping location using React JS, React Native, and Java Spring framework. Implement CRUD, JWT security, image uploading, cart system, unit testing, transaction and payment with Midtrans"
@@ -57,18 +91,7 @@ function Projects() {
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={data2}
-              isBlog={false}
-              title="Anemia Checkup Modelling & Interface"
-              description="Build simple Tkinter interface of anemia checkup using Support Vector Machine modelling to create accurate prediction of anemia based on their blood and healthy condition"
-              ghLink="https://github.com/SayyidNurCahyo/Anemia-checkup-interface"
-              demoLink=""
-            />
-          </Col>
-
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={data3}
+              imgPath={[data3]}
               isBlog={false}
               title="Acute Lymphoblastic Leukemia Classification"
               description="Build machine learning model to classify patient severity of Acute Lymphoblastic Leukemia based on their blood sample using Convolutional Neural Network with > 98% accuracy"
@@ -79,22 +102,11 @@ function Projects() {
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={data4}
+              imgPath={[data4]}
               isBlog={false}
               title="COVID-19 Patient Severity Classification"
               description="Build machine learning model to classify patient severity of COVID-19 based on their healthy checkup condition using model improvement of hybrid model Support Vector Machine and Random Forest + kNN imputation, case study in Airlangga University Hospital Surabaya"
               ghLink="https://github.com/SayyidNurCahyo/Klasifikasi_Keparahan_COVID_Using_Hybrid_RFSVM"
-              demoLink=""
-            />
-          </Col>
-
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={data6}
-              isBlog={false}
-              title="Weather Prediction Modelling"
-              description="Build machine learning model to predict the weather based on wind condition, humidity, temperature, and others factor using K-Fold Cross Validation and Neural Network in Python"
-              ghLink="https://github.com/SayyidNurCahyo/Weather_Prediction_Using_Kfold_Neural_Network"
               demoLink=""
             />
           </Col>
